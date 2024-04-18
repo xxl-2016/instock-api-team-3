c0954762913d8f77b1f5c1393ef21bab9ffe3004
Xixi Liang <lxx260905@gmail.com>
2024-04-18 16:02:59 -0700

    finalize API put/edit and add cros in index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 6060;
