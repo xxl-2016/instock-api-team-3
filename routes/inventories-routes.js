37db1ed3b6abec4765ae268463b475f6d446c6b3
Robyn Shanks <robynshanks35@gmail.com>
2024-04-18 21:49:46 -0700

    created back end API to POST/CREATE a new inventory item, data inserted using knex, validation created for request body, error codes set up

--- routes/inventories-routes.js
+++ routes/inventories-routes.js
@@ -11,4 +11,43 @@ router.get("/", async (_req, res) => {
   }
 });
 
+
+router.post("/", async (req, res) => {
+  try {
+    const { warehouse_id, item_name, description, category, status, quantity } = req.body;
+    
+    if (!warehouse_id || !item_name || !description || !category || !status || !quantity) {
+      return res.status(400).json("All fields are required");
+    }
+
+    if (isNaN(quantity)) {
+      return res.status(400).json("Quantity needs to be a number");
+    }
+    
+    const warehouseExists = await knex("warehouses").where({ id: warehouse_id }).first();
+    if (!warehouseExists) {
+      return res.status(400).json("Invalid warehouse_id");
+    }
+
+
+    
+    const [id] = await knex("inventories").insert({
+      warehouse_id,
+      item_name,
+      description,
+      category,
+      status,
+      quantity
+    });
+
+    
+    const [newInventoryItem] = await knex("inventories").where({ id });
+
+
+    res.status(201).json(newInventoryItem);
+  } catch (error) {
+    res.status(400).json(`Error creating inventory item: ${error}`);
+  }
+});
+
 module.exports = router;
