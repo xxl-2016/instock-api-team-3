169056e6ef0178eb6162285417434442e1cbb691
Robyn Shanks <robynshanks35@gmail.com>
2024-04-18 22:38:31 -0700

    Created a backend API to PUT/EDIT an inventory item, data inserted using knex, validation created for request body data, status codes created,

--- routes/inventories-routes.js
+++ routes/inventories-routes.js
@@ -50,4 +50,49 @@ router.post("/", async (req, res) => {
   }
 });
 
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { warehouse_id, item_name, description, category, status, quantity } = req.body;
+
+   
+    if (!warehouse_id || !item_name || !description || !category || !status || !quantity) {
+      return res.status(400).json("No fields can be left empty");
+    }
+
+    if (isNaN(quantity)) {
+      return res.status(400).json("Only numbers are accepted");
+    }
+
+
+    const warehouseExists = await knex("warehouses").where({ id: warehouse_id }).first();
+    if (!warehouseExists) {
+      return res.status(400).json("ID does not exist in warehouse");
+    }
+
+   
+    const existingInventory = await knex("inventories").where({ id }).first();
+    if (!existingInventory) {
+      return res.status(404).json("This inventory item is not found");
+    }
+
+    
+    await knex("inventories").where({ id }).update({
+      warehouse_id,
+      item_name,
+      description,
+      category,
+      status,
+      quantity: parseInt(quantity)
+    });
+
+
+    const updatedInventoryItem = await knex("inventories").where({ id }).first();
+    res.status(200).json(updatedInventoryItem);
+  } catch (error) {
+    res.status(400).json(`Problem updating item in inventory: ${error}`);
+  }
+});
+
 module.exports = router;
