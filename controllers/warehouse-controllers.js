e100d04f2ff0d28bd952b1676a175267ee9b8fd1
Iuliia Kositcina <iuliiakors@gmail.com>
2024-04-21 15:08:49 -0400

    Fix typo in warehouse-controllers

--- controllers/warehouse-controllers.js
+++ controllers/warehouse-controllers.js
@@ -1,26 +1,26 @@
-const knex = require('knex')(require('../knexfile'));
+const knex = require("knex")(require("../knexfile"));
 
 const findOne = async (req, res) => {
-    try {
-        const warehouseFound = await knex('warehouses')
-            .where({ id: req.params.id})  
-            .first(); 
-        
-        if(!warehouseFound) {
-            return res.status(404).json({
-                message: `Inventory with ID ${req.params.id} not found` 
-              });
-        }
+  try {
+    const warehouseFound = await knex("warehouses")
+      .where({ id: req.params.id })
+      .first();
 
-        res.status(200).json(warehouseFound);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: 'Error getting warehouse',
-        });
+    if (!warehouseFound) {
+      return res.status(404).json({
+        message: `Warehouse with ID ${req.params.id} not found`,
+      });
     }
+
+    res.status(200).json(warehouseFound);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error getting warehouse",
+    });
+  }
 };
 
 module.exports = {
-    findOne,
-    };
+  findOne,
+};
